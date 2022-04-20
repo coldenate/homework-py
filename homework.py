e6fb4888bcbf0888039da8edb17e1f7e6f20c679
colderinit <66754842+colderinit@users.noreply.github.com>
2022-04-20 17:28:33 -0500

    added somemore scaffolding

--- homework.py
+++ homework.py
@@ -5,11 +5,30 @@ import humanize
 import recurring_ical_events
 import requests
 from icalendar import Calendar, Event
+import urllib
 
 assignments = Calendar.from_ical(urllib.request.urlopen("link").read())
 
+class Subject:
+    pass
+
+class Test:
+    pass
+class Quiz:
+    pass
+# an example of what I am looking for in hese classes
+    # def __init__(self, name, due_date, points, weight):
+    #     self.name = name
+    #     self.due_date = due_date
+    #     self.points = points
+    #     self.weight = weight
+
+    # def __str__(self):
+    #     return f"{self.name} ({self.due_date})"
+
 class Assignment:
-    def __init__(assignment, name, age):
+    def __init__(assignment, name, description, due_date):
         assignment.title = name
         assignment.description = description
         assignment.due_date = due_date
+
