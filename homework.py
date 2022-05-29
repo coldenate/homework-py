549a6c37c6fdd7d44e5c73374e5cc975b020652e
colderinit <66754842+colderinit@users.noreply.github.com>
2022-05-29 14:38:40 -0500

    added test ics files, implemented sync function

--- homework.py
+++ homework.py
@@ -12,26 +12,80 @@ link = "https://har-tx.moodle.renweb.com/calendar/export_execute.php?userid=509&
 
 assignments = Calendar.from_ical(urllib.request.urlopen(link).read())
 
-class Subject: 
-    def __init__(name, level, description):
-        pass
-    pass
 
-class Test:
-    pass
-class Quiz:
-    def __init__(self, name, due_date, points, weight):
+class Student:
+    def __init__(self, name: str, provider: str, email: str = None, works: list = None):
         self.name = name
+        self.email = email
+        self.provider = provider
+        self.works: list = works
+
+    def sync(self, provider, rangetype: int):
+        stt = dt.today() - dt.timedelta(days=1)  # yesterday
+        yesterday = stt.strftime("%Y, %m, %d")
+        fourtdayslater = dt.today() + dt.timedelta(days=14)
+        truefourt = fourtdayslater.strftime("%Y, %m, %d")
+        start_date = tuple(map(int, yesterday.split(", ")))
+        end_date = tuple(map(int, truefourt.split(", ")))
+        # we create a window of 14 days later to view the calendar.
+        humanstart = stt.strftime("%B %d, %Y")  # human readable starttime
+        humanend = fourtdayslater.strftime("%B %d, %Y")  # human readable endtime
+
+        events = recurring_ical_events.of(assignments).between(start_date, end_date)
+        if len(events) == 0:
+            return False
+        if len(events) > 0:
+            works = []
+            for event in events:
+                event_name = event["SUMMARY"]
+                event_subject = event["CATEGORIES"]
+
+                # description = event["DESCRIPTION"]
+                # if description == None:
+                #     description = "No description"
+                event_starttime = event["DTSTART"].dt
+                starttime_formatted = event_starttime.strftime("%B %d, %Y")
+                event_description = event["DESCRIPTION"]
+                dict = {
+                    "event_name": event_name,
+                    "event_subject": event_subject,
+                    "event_starttime": starttime_formatted,
+                    "event_description": event_description,
+                }
+                work = Work(
+                    name=event_name,
+                    description=event_description,
+                    due_date=starttime_formatted,
+                    subject=Subject(name=event_subject),
+                )
+                self.works.append(dict)
+
+
+class Subject(Student):
+    def __init__(self, name: str = None, description: str = None):
+        self.name = name
+        self.description = description
+
+    pass
+
+
+class Work(Student):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        due_date: dt.datetime,
+        subject: Subject,
+        point_weight: int = None,
+        completed: bool = False,
+        grade: float = None,
+        teacher: Student = None,
+    ):
+        self.title = name
+        self.description = description
         self.due_date = due_date
-        self.points = points
-        self.weight = weight
-
-    def __str__(self):
-        return f"{self.name} ({self.due_date})"
-
-class Assignment:
-    def __init__(assignment, name, description, due_date):
-        assignment.title = name
-        assignment.description = description
-        assignment.due_date = due_date
-
+        self.point_weight = point_weight
+        self.subject = subject
+        self.completed = completed
+        self.grade = grade
+        self.teacher = teacher
