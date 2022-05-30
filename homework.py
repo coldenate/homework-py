e1c262c2b48c038efa6108611b743e518b0ed739
colderinit <66754842+colderinit@users.noreply.github.com>
2022-05-30 17:38:45 -0500

    support for varying ranges

--- homework.py
+++ homework.py
@@ -14,6 +14,7 @@ from icalendar import Calendar, Event
 from rich.console import Console
 from rich.table import Table
 
+# TODO: Update to python 3.10
 link = "https://har-tx.moodle.renweb.com/calendar/export_execute.php?userid=509&authtoken=afe92bc34dfe7877aec562349a943f3ec47447a6&preset_what=all&preset_time=recentupcoming"
 
 
@@ -23,6 +24,77 @@ class Student:
         self.email = email
         self.provider = provider
         self.works: list = works
+
+    def convert_to_dict(self, save_space: bool = False) -> dict:
+        """Convert all attributes of student to dictionary, but if save_space is true, take it out of the dictionary.
+        :param save_space: If True, the dictionary will omit any works to save space. This is great for databases."""
+        if save_space:
+            return {
+                "name": self.name,
+                "email": self.email,
+                "provider": self.provider,
+            }
+        if save_space == False:
+            works = []
+            for work in self.works:
+                works.append(work.convert_to_dict())
+            return {
+                "name": self.name,
+                "email": self.email,
+                "provider": self.provider,
+                "works": works,
+            }
+
+    def some_random_calc(self):
+        stt = dt.date.today() - dt.timedelta(days=1)  # yesterday
+        yesterday = stt.strftime("%Y, %m, %d")
+        fourtdayslater = dt.date.today() + dt.timedelta(days=14)
+        truefourt = fourtdayslater.strftime("%Y, %m, %d")
+        start_date = tuple(map(int, yesterday.split(", ")))
+        end_date = tuple(map(int, truefourt.split(", ")))
+
+    def datetime_to_tuple(self, datetime: dt.datetime) -> tuple:
+        un_tupled_start_date = datetime.strftime("%Y, %m, %d")
+        start_date = tuple(map(int, un_tupled_start_date.split(", ")))
+        return start_date
+
+    def calculate_timeframe(self, rangetype: int = None) -> tuple | list:
+        """Function that creates a timeframe
+        :param rangetype: 1 for today, 2 for tomorrow, 3 for this week, 4 for next week, 5 for this month, 6 for next month"""
+        if rangetype == 1:
+            raw_format = dt.datetime.today()
+            return self.datetime_to_tuple(raw_format)
+        if rangetype == 2:
+            raw_format = dt.datetime.today() + dt.timedelta(days=1)
+            return self.datetime_to_tuple(raw_format)
+        if rangetype == 3:
+            ranges = []
+            raw_start_time = dt.datetime.today()
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=6)
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+        if rangetype == 4:
+            ranges = []
+            raw_start_time = dt.datetime.today() + dt.timedelta(days=7)
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=13)
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+        if rangetype == 5:
+            ranges = []
+            raw_start_time = dt.datetime.today()
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=30)
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+        if rangetype == 6:
+            ranges = []
+            raw_start_time = dt.datetime.today() + dt.timedelta(days=31)
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=60)
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
 
     def cli_display_works(self):
         """display all works in a table view using rich library"""
@@ -38,9 +110,14 @@ class Student:
         console = Console()
         console.print(table)
 
-    def sync(self, rangetype: int):
+    def sync(
+        self, range_start: tuple = None, range_end: tuple = None, rangetype: int = None
+    ):
         """Function syncs Works Object type with cloud calendar file provider
-        :param provider: url of calendar file provider"""
+        :param provider: url of calendar file provider
+        :param range_start: tuple of start date ex. (2020, 1, 2) --> (year, month, day)
+        :param range_end: tuple of end date
+        :param rangetype: None for no preconfig, 0 for all, 1 for today, 2 for tomorrow, 3 for this week, 4 for next week, 5 for this month, 6 for next month"""
 
         try:
             raw_works = Calendar.from_ical(urllib.request.urlopen(self.provider).read())
@@ -49,33 +126,58 @@ class Student:
                 "Error fetching raw works. (Calendar file could not be reached or accessed.) | Possible fixes include checking internet connection."
             )
             raw_works = None
-        stt = dt.date.today() - dt.timedelta(days=1)  # yesterday
-        yesterday = stt.strftime("%Y, %m, %d")
-        fourtdayslater = dt.date.today() + dt.timedelta(days=14)
-        truefourt = fourtdayslater.strftime("%Y, %m, %d")
-        start_date = tuple(map(int, yesterday.split(", ")))
-        end_date = tuple(map(int, truefourt.split(", ")))
-        # we create a window of 14 days later to view the calendar.
-        humanstart = stt.strftime("%B %d, %Y")  # human readable starttime
-        humanend = fourtdayslater.strftime("%B %d, %Y")  # human readable endtime
 
-        events = recurring_ical_events.of(raw_works).between(
-            (2022, 5, 1), (2022, 5, 15)
-        )
+        if range_start is None and range_end is None or rangetype is 0:
+            events = recurring_ical_events.of(raw_works).all()
+        if range_start is not None and range_end is not None:
+            events = recurring_ical_events.of(raw_works).between(range_start, range_end)
+        if rangetype == 0:
+            events = recurring_ical_events.of(raw_works).all()
+        if rangetype == 1:
+            events = recurring_ical_events.of(raw_works).at(self.calculate_timeframe(1))
+        if rangetype == 2:
+            events = recurring_ical_events.of(raw_works).at(self.calculate_timeframe(2))
+        if rangetype == 3:
+            ranges = self.calculate_timeframe(3)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+        if rangetype == 4:
+            ranges = self.calculate_timeframe(4)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+        if rangetype == 5:
+            ranges = self.calculate_timeframe(5)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+        if rangetype == 6:
+            ranges = self.calculate_timeframe(6)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+
         if len(events) == 0:
             return False
         if len(events) > 0:
             works = []
             for event in events:
-                event_name = event["SUMMARY"]
-                event_subject = event["CATEGORIES"].cats[0]  # the first category found
+                try:
+                    event_name = event["SUMMARY"]
+                except:
+                    event_name = "No Title"
+                try:
+                    event_subject = event["CATEGORIES"].cats[
+                        0
+                    ]  # the first category found
+                except:
+                    event_subject = "No Subject"
+                try:
+                    event_starttime = event["DTSTART"].dt
+                    starttime_formatted = event_starttime.strftime("%B %d, %Y")
+                except:
+                    event_starttime = "No Start Time"
+                try:
+                    event_description = event["DESCRIPTION"]
+                except:
+                    event_description = "No Description"
 
-                # description = event["DESCRIPTION"]
-                # if description == None:
-                #     description = "No description"
-                event_starttime = event["DTSTART"].dt
-                starttime_formatted = event_starttime.strftime("%B %d, %Y")
-                event_description = event["DESCRIPTION"]
+                    # description = event["DESCRIPTION"]
+                    # if description == None:
+                    #     description = "No description"
                 dict = {
                     "event_name": event_name,
                     "event_subject": event_subject,
@@ -109,7 +211,7 @@ class Work(Student):
         point_weight: int = None,
         completed: bool = False,
         grade: float = None,
-        teacher: Student = None,
+        teacher=None,
     ):
         self.title = name
         self.description = description
@@ -119,3 +221,16 @@ class Work(Student):
         self.completed = completed
         self.grade = grade
         self.teacher = teacher
+
+    def convert_to_dict(self) -> dict:
+        """convert each work into a dictionary"""
+        return {
+            "title": self.title,
+            "description": self.description,
+            "due_date": self.due_date,
+            "subject": self.subject.name,
+            "point_weight": self.point_weight,
+            "completed": self.completed,
+            "grade": self.grade,
+            "teacher": self.teacher,
+        }
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-
 import homework
 
-link = "https://har-tx.moodle.renweb.com/calendar/export_execute.php?userid=509&authtoken=afe92bc34dfe7877aec562349a943f3ec47447a6&preset_what=all&preset_time=monthnow"
-
+link1 = "https://har-tx.moodle.renweb.com/calendar/export_execute.php?userid=509&authtoken=afe92bc34dfe7877aec562349a943f3ec47447a6&preset_what=all&preset_time=monthnow"
+link = "https://calendar.google.com/calendar/ical/njips87tv2foa0cdeehge6u17c%40group.calendar.google.com/private-eae13d44ba261de9f28664ded6223358/basic.ics"
 ns = homework.Student("Nate_Solis", link)
 
-ns.sync(1)
+# ns.sync((2022, 5, 1), (2022, 5, 15))
+#
+# # collect data from the link
 
+ns.sync(rangetype=6)
 ns.cli_display_works()
 
 
+# print(f"True:{ns.convert_to_dict(True)}")  # convert
+# print(f"False:{ns.convert_to_dict()}")  # convert
