23cea8c96f9a6d1e1f865611fdd105799b2e2ad0
Nathan Solis <66754842+colderinit@users.noreply.github.com>
2022-06-02 22:16:22 -0500

    The first version home... **works.** (#2)
    * conform to usability
    
    * fixed syntax errors and import sorts
    
    * added test ics files, implemented sync function
    
    * basic implementation of importing calendar files
    
    * wrote an essay for the readme
    
    * implemented rich display
    
    * fixed category issue
    
    * added sxception for lck of internet connectton
    
    * updated python version to 3.10
    
    * support for varying ranges
    
    * Fixed footnotes
    
    * fixed formatting, writing, and cleaned up reqs
    
    * final commit for first iteration, personal info discarded

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # homework-py
-A python package that parses moodle assignments and events using calendar files 
+
+Hallo!! Homework-py is an abstraction over many SIS[^1] simple ways of feeding assignment data into a student's many electronic planners.
+
+## Why is this existing when this has been solved before?
+
+It is an educational project that I had passion for, so I chose to begin it despite knowing it could bear little fruit :).
+
+**Aside from being a simple and *pointless-ish* educational project,** the main reasons I started this are found [here](reasons.md).
+
+## Awesome dependencies of this library(!!)
+
+(epicly doesn't have time to list them yet) (epicly puts on shades to express coolness factor)
+
+### TODO
+
+- [x] Basic Support for Moodle Calendar OOP Processing
+
+> From my knowledge of (nothingness...) OOP, the library should be using a very basic form of OOP as of the first release. I am not sure though. `¯\_(ツ)_/¯`
+
+- [ ] Implement Same parsing structure for Canvas LMS
+- [ ] Implement seperate data flagging for Renweb's calendar
+- [ ] Clean up code with formatting and formatting
+- [ ] Setup as a python package
+- [ ] Release! (as a standard version with correct semantic versioning)
--- homework.py
+++ homework.py
@@ -1,34 +1,226 @@
 import datetime as dt
-import json
-import aiohttp
-import humanize
-import recurring_ical_events
-import requests
-from icalendar import Calendar, Event
 import urllib
+import urllib.request
 
-link = "https://har-tx.moodle.renweb.com/calendar/export_execute.php?userid=509&authtoken=afe92bc34dfe7877aec562349a943f3ec47447a6&preset_what=all&preset_time=recentupcoming"
+import recurring_ical_events
+from icalendar import Calendar, Event
+from rich.console import Console
+from rich.table import Table
 
-assignments = Calendar.from_ical(urllib.request.urlopen(link).read())
 
-class Class: # I know, it is weird. "Class Class. I didn't want to put subject."
-    def __init__(name, level, description)
-
-class Test:
-    pass
-class Quiz:
-    def __init__(self, name, due_date, points, weight):
+class Student:
+    def __init__(self, name: str, provider: str, email: str = None, works: list = []):
         self.name = name
+        self.email = email
+        self.provider = provider
+        self.works: list = works
+
+    def convert_to_dict(self, save_space: bool = False) -> dict:
+        """Convert all attributes of student to dictionary, but if save_space is true, take it out of the dictionary.
+        :param save_space: If True, the dictionary will omit any works to save space. This is great for databases."""
+        if save_space:
+            return {
+                "name": self.name,
+                "email": self.email,
+                "provider": self.provider,
+            }
+        if save_space == False:
+            works = []
+            for work in self.works:
+                works.append(work.convert_to_dict())
+            return {
+                "name": self.name,
+                "email": self.email,
+                "provider": self.provider,
+                "works": works,
+            }
+
+    def some_random_calc(self):
+        stt = dt.date.today() - dt.timedelta(days=1)  # yesterday
+        yesterday = stt.strftime("%Y, %m, %d")
+        fourtdayslater = dt.date.today() + dt.timedelta(days=14)
+        truefourt = fourtdayslater.strftime("%Y, %m, %d")
+        start_date = tuple(map(int, yesterday.split(", ")))
+        end_date = tuple(map(int, truefourt.split(", ")))
+
+    def datetime_to_tuple(self, datetime: dt.datetime) -> tuple:
+        un_tupled_start_date = datetime.strftime("%Y, %m, %d")
+        start_date = tuple(map(int, un_tupled_start_date.split(", ")))
+        return start_date
+
+    def calculate_timeframe(self, rangetype: int = None) -> tuple | list:
+        """Function that creates a timeframe
+        :param rangetype: 1 for today, 2 for tomorrow, 3 for this week, 4 for next week, 5 for this month, 6 for next month"""
+        if rangetype == 1:
+            raw_format = dt.datetime.today()
+            return self.datetime_to_tuple(raw_format)
+        if rangetype == 2:
+            raw_format = dt.datetime.today() + dt.timedelta(days=1)
+            return self.datetime_to_tuple(raw_format)
+        if rangetype == 3:
+            ranges = []
+            raw_start_time = dt.datetime.today()
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=6)
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+        if rangetype == 4:
+            ranges = []
+            raw_start_time = dt.datetime.today() + dt.timedelta(days=7)
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=13)
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+        if rangetype == 5:
+            ranges = []
+            raw_start_time = dt.datetime.today()
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=30)
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+        if rangetype == 6:
+            ranges = []
+            raw_start_time = dt.datetime.today() + dt.timedelta(days=31)
+            raw_end_time = dt.datetime.today() + dt.timedelta(days=60)
+            ranges.append(self.datetime_to_tuple(raw_start_time))
+            ranges.append(self.datetime_to_tuple(raw_end_time))
+            return ranges
+
+    def cli_display_works(self):
+        """display all works in a table view using rich library"""
+        table = Table(title=f"{self.name}'s Works")
+        table.add_column("Name")
+        table.add_column("Description")
+        table.add_column("Due Date")
+        table.add_column("Subject")
+        for work in self.works:
+            table.add_row(
+                work.title, work.description, work.due_date, work.subject.name
+            )
+        console = Console()
+        console.print(table)
+
+    def sync(
+        self, range_start: tuple = None, range_end: tuple = None, rangetype: int = None
+    ):
+        """Function syncs Works Object type with cloud calendar file provider
+        :param provider: url of calendar file provider
+        :param range_start: tuple of start date ex. (2020, 1, 2) --> (year, month, day)
+        :param range_end: tuple of end date
+        :param rangetype: None for no preconfig, 0 for all, 1 for today, 2 for tomorrow, 3 for this week, 4 for next week, 5 for this month, 6 for next month"""
+
+        try:
+            raw_works = Calendar.from_ical(urllib.request.urlopen(self.provider).read())
+        except:
+            print(
+                "Error fetching raw works. (Calendar file could not be reached or accessed.) | Possible fixes include checking internet connection.\nThere could also be no events."
+            )
+            raw_works = None
+
+        if range_start is None and range_end is None or rangetype == 0:
+            events = recurring_ical_events.of(raw_works).all()
+        if range_start is not None and range_end is not None:
+            events = recurring_ical_events.of(raw_works).between(range_start, range_end)
+        if rangetype == 0:
+            events = recurring_ical_events.of(raw_works).all()
+        if rangetype == 1:
+            events = recurring_ical_events.of(raw_works).at(self.calculate_timeframe(1))
+        if rangetype == 2:
+            events = recurring_ical_events.of(raw_works).at(self.calculate_timeframe(2))
+        if rangetype == 3:
+            ranges = self.calculate_timeframe(3)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+        if rangetype == 4:
+            ranges = self.calculate_timeframe(4)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+        if rangetype == 5:
+            ranges = self.calculate_timeframe(5)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+        if rangetype == 6:
+            ranges = self.calculate_timeframe(6)
+            events = recurring_ical_events.of(raw_works).between(ranges[0], ranges[1])
+
+        if len(events) == 0:
+            return False
+        if len(events) > 0:
+            works = []
+            for event in events:
+                try:
+                    event_name = event["SUMMARY"]
+                except:
+                    event_name = "No Title"
+                try:
+                    event_subject = event["CATEGORIES"].cats[
+                        0
+                    ]  # the first category found
+                except:
+                    event_subject = "No Subject"
+                try:
+                    event_starttime = event["DTSTART"].dt
+                    starttime_formatted = event_starttime.strftime("%B %d, %Y")
+                except:
+                    event_starttime = "No Start Time"
+                try:
+                    event_description = event["DESCRIPTION"]
+                except:
+                    event_description = "No Description"
+
+                    # description = event["DESCRIPTION"]
+                    # if description == None:
+                    #     description = "No description"
+                dict = {
+                    "event_name": event_name,
+                    "event_subject": event_subject,
+                    "event_starttime": starttime_formatted,
+                    "event_description": event_description,
+                }
+                work = Work(
+                    name=f"{event_name}",
+                    description=f"{event_description}",
+                    due_date=f"{starttime_formatted}",
+                    subject=Subject(name=f"{event_subject}"),
+                )
+                self.works.append(work)
+
+
+class Subject(Student):
+    def __init__(self, name: str = None, description: str = None):
+        self.name = name
+        self.description = description
+
+    pass
+
+
+class Work(Student):
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        due_date: dt.datetime,
+        subject: Subject,
+        point_weight: int = None,
+        completed: bool = False,
+        grade: float = None,
+        teacher=None,
+    ):
+        self.title = name
+        self.description = description
         self.due_date = due_date
-        self.points = points
-        self.weight = weight
+        self.point_weight = point_weight
+        self.subject = subject
+        self.completed = completed
+        self.grade = grade
+        self.teacher = teacher
 
-    def __str__(self):
-        return f"{self.name} ({self.due_date})"
-
-class Assignment:
-    def __init__(assignment, name, description, due_date):
-        assignment.title = name
-        assignment.description = description
-        assignment.due_date = due_date
-
+    def convert_to_dict(self) -> dict:
+        """convert each work into a dictionary"""
+        return {
+            "title": self.title,
+            "description": self.description,
+            "due_date": self.due_date,
+            "subject": self.subject.name,
+            "point_weight": self.point_weight,
+            "completed": self.completed,
+            "grade": self.grade,
+            "teacher": self.teacher,
+        }
