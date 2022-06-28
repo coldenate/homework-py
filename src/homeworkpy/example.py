e22bc2abc0de36917199173182f3394f71f6c540
Nathan Solis <66754842+colderinit@users.noreply.github.com>
2022-06-27 20:30:30 -0700

    IMPL: Renweb Calendar+Report Card Support (#15)
    # NOTICE
    
    ```diff
    - A commit in this PR contained some of my private credentials. I made a classic blunder. 
    - :( I am so sad.
    +  I will be squashing the commits down into one for the pr. I have no idea what to do. I have looked everywhere.
    ```
    * removed some pyprojet stuff
    
    * removed old pipfiles
    
    * added new pipfiles
    
    * implemented code for direct file access
    
    * successful renweb login in jupyter notebook
    
    * impl: report card syncing
    
    * finish impl renwebReportCard Extraction
    
    * impl: multiple provider support
    
    * attempt at removing credential leak blunder
    
    * blunder protection

--- src/homeworkpy/example.py
+++ src/homeworkpy/example.py
@@ -1,17 +1,34 @@
 import homework  # importing the file that contains the objects. This is bound to change.
+from private import link, login_info
 
-link1 = "insert your link"
-link2 = "insert your link" 
-# Those are links to ics files. For now, ICS files are what we parse. 
+LINK1 = link
+LINK2 = "insert your link"
+LINK3 = "insert your link"
+# Those are links to ics files. For now, ICS files are what we parse.
 
 
+# m = input("Input 1 for homework or 2 for random ")
 
-m = input("Input 1 for homework or 2 for random ")
+ns = homework.Student(
+    "Note Salad",
+    {
+        link: True,
+        LINK2: False,
+        LINK3: False,
+    },
+    True,
+    renweb=True,
+    renweb_link="https://har-tx.client.renweb.com",
+    renwebCredentials=login_info,
+)  # This is the creation of a Student object. It is assigned to "ns".
+#  The parameter is a link to the server that will *immediately* serve the ics file.
 
-if m == "1":
-    ns = homework.Student("Note Salad", link1) # This is the creation of a Student object. It is assigned to "ns". The parameter is a link to the server that will *immediately* serve the ics file.
-if m == "2":
-    ns = homework.Student("Note Salad", link2)
 
-ns.sync() # this is the sync function. It will download the ics file from the link and parse it, and then it will sync the student object's dictionary of homework with it. (Moreso appends)
-ns.cli_display_works() # a simple test function that uses Rich to display a table of the homework objects. They are called works.
+ns.sync()
+# this is the sync function. It will download the ics file from the link and parse it, and then it
+# will sync the student object's dictionary of homework with it. (Moreso appends)
+
+ns.cli_display_works()
+# a simple test function that uses Rich to display
+# a table of the homework objects. They are called works.
+# print(ns.report_card.classes)
--- src/homeworkpy/homework.py
+++ src/homeworkpy/homework.py
@@ -1,26 +1,100 @@
+"""
+Please refer to the documentation provided in the README.md,
+which can be found at gorpyter's PyPI URL: https://pypi.org/project/gorpyter/
+"""
 import datetime as dt
+import json
 import urllib
 import urllib.request
 from functools import cache
 
 import recurring_ical_events
-from icalendar import Calendar, Event
+import requests
+from bs4 import BeautifulSoup
+from icalendar import Calendar
 from rich.console import Console
 from rich.table import Table
+from tools import cleanup_json, html_to_json, merge_data
 
 
 @cache
-def fetch_calendar(url):
-    """Fetches calendar file from url"""
-    x = urllib.request.urlopen(url)
-    return x
+def fetch_calendar(src: str, is_file: bool = False):
+    """Fetches calendar file from url,
+    :param: is_file | uses a file opener instead of a link"""
+    if is_file == False:
+        file = urllib.request.urlopen(src)
+        # return x
+        raw_works = Calendar.from_ical(file.read())
+        return raw_works
+    if is_file == True:
+        file = open(src, "r")
+        # return file
+        raw_works = Calendar.from_ical(file.read())
+        return raw_works
+
+
+class ReportCard:
+    def __init__(
+        self,
+        known_classes: list = [],
+        classes: dict = None,
+    ):
+        self.known_classes = known_classes
+        self.classes = classes
+
+    def extract(self, html):
+        """Extracts data from a report card (NOT TO BE USED OUTSIDE OF THIS LIBRARY)"""
+        content_no_thead = html
+
+        nothead = json.loads((html_to_json(content_no_thead, indent=4)))
+
+        # print("cleaned up version")
+        table_headers = cleanup_json(nothead, self.known_classes)
+        classDictonaries = merge_data(self.known_classes, table_headers)
+        self.classes = classDictonaries
+        return classDictonaries
+
 
 class Student:
-    def __init__(self, name: str, provider: str, email: str = None, works: list = []):
+    def __init__(
+        self,
+        name: str,
+        provider: dict,
+        # is_file: bool,
+        email: str = None,
+        works: list = [],
+        report_card: ReportCard = None,
+        classes: list = [],
+        renweb: bool = False,
+        renweb_link: str = None,
+        renwebCredentials: dict = {
+            "DistrictCode": None,
+            "username": None,
+            "password": None,
+            "UserType": "PARENTSWEB-STUDENT",  # I think this will break across different renweb sites...
+            "login": "Log+In",
+        },
+        renwebDisctrictCode: str = None,
+        renwebLoggedIn: bool = False,
+        renwebCalendarSync: bool = False,
+    ):
         self.name = name
         self.email = email
-        self.provider = provider
+        self.providers = provider
+        # self.providerIsFile = is_file
         self.works: list = works
+        self.report_card: ReportCard = report_card
+        self.classes = classes
+        self.renweb = renweb
+        self.renweb_link: self = renweb_link
+        self.renwebCredentials: dict = renwebCredentials
+        self.renwebDisctrictCode: str = renwebDisctrictCode
+        self.renwebSession = None
+        self.renwebCalendarSync: bool = renwebCalendarSync
+        self.renwebLoggedIn: bool = renwebLoggedIn
+
+        if self.renweb == True:
+            self.renwebSession = requests.Session()
 
     def convert_to_dict(self, save_space: bool = False) -> dict:
         """Convert all attributes of student to dictionary, but if save_space is true, take it out of the dictionary.
@@ -29,7 +103,7 @@ class Student:
             return {
                 "name": self.name,
                 "email": self.email,
-                "provider": self.provider,
+                "provider": self.providers,
             }
         if save_space == False:
             works = []
@@ -38,9 +112,42 @@ class Student:
             return {
                 "name": self.name,
                 "email": self.email,
-                "provider": self.provider,
+                "provider": self.providers,
                 "works": works,
             }
+
+    def renwebLogin(self) -> requests.Session:
+        """Logs into the renweb website"""
+        login_info = self.renwebSession.post(
+            self.renweb_link + "/pwr/index.cfm", data=self.renwebCredentials
+        )
+        self.renwebLoggedIn = True
+
+    def import_card_from_renweb(self):
+
+        if self.renwebLoggedIn != True:
+            self.renwebLogin()
+
+        reportCardMain = self.renwebSession.get(
+            self.renweb_link + "/pwr/student/report-card.cfm"
+        )
+
+        soup = BeautifulSoup(reportCardMain.content, "html.parser")
+        NASReportCardElement = soup.find_all("iframe", {"class": "gframe"})
+
+        reportCardLocation = NASReportCardElement[0].attrs["src"]
+
+        report_card_request = self.renwebSession.get(
+            self.renweb_link + reportCardLocation
+        )
+
+        reportCardHTML = report_card_request.content
+
+        self.report_card = ReportCard()
+
+        self.report_card.extract(reportCardHTML)
+
+        self.renwebSession.close()
 
     def some_random_calc(self):
         stt = dt.date.today() - dt.timedelta(days=1)  # yesterday
@@ -107,17 +214,20 @@ class Student:
         console = Console()
         console.print(table)
 
-    def sync(
-        self, range_start: tuple = None, range_end: tuple = None, rangetype: int = None
+    def process_append_ical_file(
+        self,
+        link: str,
+        range_start: tuple,
+        range_end: tuple,
+        rangetype: int,
+        isFile: bool,
     ):
-        """Function syncs Works Object type with cloud calendar file provider
-        :param provider: url of calendar file provider
-        :param range_start: tuple of start date ex. (2020, 1, 2) --> (year, month, day)
-        :param range_end: tuple of end date
-        :param rangetype: None for no preconfig, 0 for all, 1 for today, 2 for tomorrow, 3 for this week, 4 for next week, 5 for this month, 6 for next month"""
-
+        """This function is designed to be iterable.
+        NOT TO BE USED OUTSIDE OF LIBRARY"""
         try:
-            raw_works = Calendar.from_ical(fetch_calendar(self.provider).read())
+            # raw_works = Calendar.from_ical(fetch_calendar(self.provider).read())
+            # this is the main kicker, or the ol can o' beans. It throws around the calendar file
+            raw_works = fetch_calendar(link, is_file=isFile)
         except:
             print(
                 "Error fetching raw works. (Calendar file could not be reached or accessed.) | Possible fixes include checking internet connection.\nThere could also be no events."
@@ -188,6 +298,31 @@ class Student:
                     subject=Subject(name=f"{event_subject}"),
                 )
                 self.works.append(work)
+
+    def sync(
+        self,
+        range_start: tuple = None,
+        range_end: tuple = None,
+        rangetype: int = None,
+    ):
+        """Function syncs Works Object type with cloud calendar file provider
+        :param provider: url of calendar file provider
+        :param range_start: tuple of start date ex. (2020, 1, 2) --> (year, month, day)
+        :param range_end: tuple of end date
+        :param rangetype: None for no preconfig, 0 for all, 1 for today, 2 for tomorrow, 3 for this week, 4 for next week, 5 for this month, 6 for next month"""
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SYNCING CALENDARS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        for provider in self.providers:
+            x =self.providers[provider]
+            self.process_append_ical_file(
+                provider,
+                range_start,
+                range_end,
+                rangetype,
+                isFile=self.providers[provider],
+            )
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SYNCING REPORT CARDS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        if self.renweb == True:
+            self.import_card_from_renweb()
 
 
 class Subject(Student):
