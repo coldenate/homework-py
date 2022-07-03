bb790f2a765193933c2a35da873fba3e19205436
Nathan Solis <66754842+colderinit@users.noreply.github.com>
2022-07-03 22:35:04 +0000

    poor copypasta

--- src/homeworkpy/homework.py
+++ src/homeworkpy/homework.py
@@ -1,6 +1,5 @@
 """
 Please refer to the documentation provided in the README.md,
-which can be found at gorpyter's PyPI URL: https://pypi.org/project/gorpyter/
 """
 import datetime as dt
 import json
